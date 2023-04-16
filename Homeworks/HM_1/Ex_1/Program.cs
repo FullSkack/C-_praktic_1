150f5d32e30226f54a9b00e744c85628e5ff1390
Martin_Power <f.drindrozhik@yandex.ru>
2023-04-17 00:40:39 +0300

    add ex_1

---
+++ Homeworks/HM_1/Ex_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = 5;
+int b = 7;
+
+if (a > b )
+{
+  Console.Write("max = ");
+  Console.WriteLine(a);
+  Console.Write("min = ");
+  Console.WriteLine(b);   
+}
+else
+{
+  Console.Write("max = ");
+  Console.WriteLine(b);
+  Console.Write("min = ");
+  Console.WriteLine(a);  
+}
