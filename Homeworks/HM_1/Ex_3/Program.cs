ce4abae169f0c7ec6159ae78aa49f48966a97e83
Martin_Power <f.drindrozhik@yandex.ru>
2023-04-22 13:33:27 +0300

    Задание с семинара

---
+++ Homeworks/HM_1/Ex_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int numString = int.Parse(Console.ReadLine());
+decimal numDouble = Convert.ToDecimal(numString);
+decimal remainder = numDouble % 2;
+if (remainder == 0)
+{
+    Console.WriteLine("это четное число");
+}
+else
+{
+    Console.WriteLine("это не четное число");
+}
---
+++ Seminar/Sem_1/Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая на въоди принимает число и выдаёт его квадрат.
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"{number} -> {sqr}");
