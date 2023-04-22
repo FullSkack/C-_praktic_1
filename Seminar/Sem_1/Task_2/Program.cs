689ff484747c2254c9d9a06834d4c985711f55dd
Martin_Power <f.drindrozhik@yandex.ru>
2023-04-22 14:09:27 +0300

    Задание с семинара

---
+++ Seminar/Sem_1/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("введите первое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+if (number_1 == number_2 * number_2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
