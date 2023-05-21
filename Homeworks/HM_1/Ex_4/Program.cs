31bc876c44a80d3a9e4bbc0c2f6cc2f8a8f8ffc0
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-21 17:18:15 +0300

    Ex_4_Update

---
+++ Homeworks/HM_1/Ex_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.Write("введите какое-нибудь целое число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i+ ", ");
+        }
+    }
