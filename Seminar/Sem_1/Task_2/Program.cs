0dc008563444119bea42553806181bbe32e0f68a
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-27 21:17:29 +0300

    commit

--- Seminar/Sem_1/Task_2/Program.cs
+++ Seminar/Sem_1/Task_2/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("введите первое число: ");
-int number_1 = int.Parse(Console.ReadLine())!;
+int number_1 = int.Parse(Console.ReadLine());
 Console.Write("введите первое число: ");
-int number_2 = int.Parse(Console.ReadLine())!;
+int number_2 = int.Parse(Console.ReadLine());
 if (number_1 == number_2 * number_2)
 {
     Console.WriteLine("Первое число является квадратом второго");
