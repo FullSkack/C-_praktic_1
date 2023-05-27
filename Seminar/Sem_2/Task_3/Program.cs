2c325b399a0c603081b9fe371db5034fbf51b0a5
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-27 21:46:07 +0300

    commit

--- Seminar/Sem_2/Task_3/Program.cs
+++ Seminar/Sem_2/Task_3/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Выводит случайное трехзначное число и удаляет 2-ю цифру этого числа
 Console.Clear();
-int num = new Random().Next(10, 100);
+int num = new Random().Next(100, 1000); // ниржняя граница входит, верхняя не входит
+System.Console.WriteLine($"Было сгенерированно число {num}");
+
+int x1 = num/100;
+int x3 = num%10;
+
+System.Console.WriteLine($"{x1}{x3}");
