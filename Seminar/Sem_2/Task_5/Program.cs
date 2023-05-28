0e8231adbae363fd2decad7bef474a817eec3282
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-28 22:00:06 +0300

    commit

--- Seminar/Sem_2/Task_5/Program.cs
+++ Seminar/Sem_2/Task_5/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Пользователь вводит число, программа 
 // проверяет кратно ли оно одновременно 7 и 23
 Console.Clear();
-System.Console.Write($"Введите число: ");
+System.Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 if (num%7==0 && num%23 == 0)
