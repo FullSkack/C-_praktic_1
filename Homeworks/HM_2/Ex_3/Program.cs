3c9b6f2780e6dccc95cff7e7303abba0c9f7d738
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-28 22:20:36 +0300

    commit

---
+++ Homeworks/HM_2/Ex_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+System.Console.Write("Введите цифру от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 || num == 7)
+    System.Console.WriteLine("Да");
+else if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5)
+    System.Console.WriteLine("Нет");
+else
+    System.Console.WriteLine("Неверные параметы. Введите цифру от 1 до 7: ");
