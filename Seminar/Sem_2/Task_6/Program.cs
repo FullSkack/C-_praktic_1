a0309dface11ce842b52941b23ec3eb4c05a829b
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-28 20:34:29 +0300

    commit

---
+++ Seminar/Sem_2/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу, которая принимает два чилса и проверяет, 
+// являются ли они квадратми друг дурга
+Console.Clear();
+Console.Write("Введите первое число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+if (x1 == x2*x2 || x2 == x1*x1)
+    System.Console.WriteLine("Да");
+else
+    System.Console.WriteLine("Нет");
