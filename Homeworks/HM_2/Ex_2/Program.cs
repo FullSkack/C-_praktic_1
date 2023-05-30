89e21450c3bfdec966072f219d3776ed8443e025
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-30 07:12:28 +0300

    commit

--- Homeworks/HM_2/Ex_2/Program.cs
+++ Homeworks/HM_2/Ex_2/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
