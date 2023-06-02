bf36cd3b370f2784c065316f80eeacf79949b5c9
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-02 21:24:58 +0300

    commit

--- Homeworks/HM_2/Ex_2/Program.cs
+++ Homeworks/HM_2/Ex_2/Program.cs
@@ -1,4 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
 
+if (count < 3)
+{
+    Console.Write("Третьей цифры нет");
+}
+
+else
+{
+    int MakeArray(int a, int b)
+    {
+    int result = 0;
+    int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+             c = c * 10;
+        }
+        result = (a/c) % 10;
+    return result;    
+    }
+    Console.Write(MakeArray(number, count));
+    
+}
