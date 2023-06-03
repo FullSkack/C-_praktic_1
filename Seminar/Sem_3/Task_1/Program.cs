cdbf13092482a584861daafccf3fc2db515a9947
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-03 17:58:42 +0300

    commit

---
+++ Seminar/Sem_3/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает на вход координаты точки (x, y)
+// x!=0 y!=0, программа выдаёт номер четверти плоскости точки
+Console.Clear();
+System.Console.WriteLine("Введите координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>0 && y>0)
+    System.Console.WriteLine("Четветь #1");
+if (x<0 && y>0)
+    System.Console.WriteLine("Четветь #2");
+if (x<0 && y<0)
+    System.Console.WriteLine("Четветь #3");
+if (x>0 && y<0)
+    System.Console.WriteLine("Четветь #4");  
