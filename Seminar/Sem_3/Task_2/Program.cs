cf46868e7ad36810b26570b27395d5fb7ab790d6
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-04 16:38:22 +0300

    commit

---
+++ Seminar/Sem_3/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int CheckKoord_2(int x, int y) // функция 
+{
+    int result =0;
+
+    if (x>0 && y>0)
+        result = 1;
+    else if (x<0 && y>0)
+        result = 2;
+    else if (x<0 && y<0)
+        result = 3;
+    else if (x>0 && y<0)
+        result = 4;    
+    return result; 
+
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"это четверть #{CheckKoord_2(x,y)}");
