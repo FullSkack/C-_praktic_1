fc5840016043695310bb4a18ca32c9cb52a1b6aa
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-04 20:56:09 +0300

    commit

---
+++ Seminar/Sem_3/Task_5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принимает на вход любое число и выдает квадтраты от 1 до N
+void PrintSqrt(int N)
+{
+    int count = 1;
+
+    while (count <= N)
+    {
+        int sqrt = count*count;
+        System.Console.Write($"{sqrt}; ");
+        count ++;
+    }
+    
+}
+
+Console.Clear();
+System.Console.Write("Здайте целое положительное число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintSqrt(N);
+
