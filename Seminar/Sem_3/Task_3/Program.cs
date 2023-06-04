6138cbd8797bdd213849f905106c2a813d13103d
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-04 17:30:49 +0300

    commit

---
+++ Seminar/Sem_3/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// по номеру четверти выдать диапазон значений
+void Range(int quart) // процедура соверщающая действия над данными, ничего не возвращает
+{
+    if (quart == 1)
+        System.Console.WriteLine("x > 0, y > 0");
+    else if (quart == 2)
+        System.Console.WriteLine("x < 0, y > 0");
+    else if (quart == 3)
+        System.Console.WriteLine("x < 0, y < 0");
+    else if (quart == 4)
+        System.Console.WriteLine("x > 0, y < 0");  
+    else
+        System.Console.WriteLine("Некоректное значение, введие 1,2,3 или 4");
+}
+
+Console.Clear();
+System.Console.Write("Введите номер координатной четверти: ");
+int quart = Convert.ToInt32(Console.ReadLine());
+
+Range(quart);
+
+
