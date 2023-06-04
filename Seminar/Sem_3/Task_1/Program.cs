5273c8967a014bdecc06462068545f10bec20f96
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-04 15:31:18 +0300

    commit

--- Seminar/Sem_3/Task_1/Program.cs
+++ Seminar/Sem_3/Task_1/Program.cs
@@ -1,5 +1,19 @@
 ﻿// Программа принимает на вход координаты точки (x, y)
 // x!=0 y!=0, программа выдаёт номер четверти плоскости точки
+void CheckKoord(int x, int y) // процедура соверщающая действия над данными, ничего не возвращает
+{
+    if (x>0 && y>0)
+        System.Console.WriteLine("Четветь #1");
+    else if (x<0 && y>0)
+        System.Console.WriteLine("Четветь #2");
+    else if (x<0 && y<0)
+        System.Console.WriteLine("Четветь #3");
+    else if (x>0 && y<0)
+        System.Console.WriteLine("Четветь #4");  
+    else
+        System.Console.WriteLine("Точка находится в начале координат");
+}
+
 Console.Clear();
 System.Console.WriteLine("Введите координату x: ");
 int x = Convert.ToInt32(Console.ReadLine());
@@ -7,11 +21,5 @@ int x = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите координату y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if (x>0 && y>0)
-    System.Console.WriteLine("Четветь #1");
-if (x<0 && y>0)
-    System.Console.WriteLine("Четветь #2");
-if (x<0 && y<0)
-    System.Console.WriteLine("Четветь #3");
-if (x>0 && y<0)
-    System.Console.WriteLine("Четветь #4");  
+CheckKoord(x, y);
+
