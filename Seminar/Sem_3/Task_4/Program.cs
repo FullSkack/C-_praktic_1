6f859c6ecd1d5cd0d20be8ba26573cd8153edae5
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-04 19:48:57 +0300

    commit

---
+++ Seminar/Sem_3/Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа принимает координаты двух точек и находт расстояние между ними.
+double Lengh(double x1, double y1, double x2, double y2)
+{
+    double result = Math.Round(Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)), 2);
+    return result;
+}
+
+System.Console.Write("Введите x1 = ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите y1 = ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите x2 = ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите y2 = ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+ 
+System.Console.WriteLine($"Длина отрезка = {Lengh(x1, y1, x2, y2)}");
