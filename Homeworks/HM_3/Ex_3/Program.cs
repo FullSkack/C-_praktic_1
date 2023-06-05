36eb2a31e34bb1cc41a00ea524c5dbca3a6b955a
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-05 22:15:37 +0300

    commit

---
+++ Homeworks/HM_3/Ex_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void PrintCube(int N)
+{
+    int count = 1;
+
+    while (count <= N)
+    {
+        int cube = count*count*count;
+        System.Console.Write($"{cube}; ");
+        count ++;
+    }
+    
+}
+
+Console.Clear();
+System.Console.Write("Здайте целое положительное число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintCube(N);
