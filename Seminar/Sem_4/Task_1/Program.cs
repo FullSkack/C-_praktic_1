63c739baa85bc9a205cf05f2c39f781d69be8e79
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-10 18:25:51 +0300

    commit

---
+++ Seminar/Sem_4/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Функция на вход принимает N, выдает сумму от 1 до N
+int Summ(int num)
+{
+    int result = 0;
+    for (int i=1; i<=num;i++)
+        result+=i;
+    return result;
+}
+
+Console.Clear();
+System.Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма чисел от 1 до {num} равна {Summ(num)}");
