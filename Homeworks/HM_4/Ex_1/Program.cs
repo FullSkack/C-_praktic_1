5960b37df74dc8a629a58ea6bfce89909a3aeb10
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-11 01:05:38 +0300

    commit

--- Homeworks/HM_4/Ex_1/Program.cs
+++ Homeworks/HM_4/Ex_1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите число A = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B = ");
+double b = Convert.ToDouble(Console.ReadLine());
+double c = 1;
+if (b<1)
+Console.Write("Число B должно быть натуральным");
+else
+{
+    while (b != 0)
+    {
+        c = Math.Pow(a, b);
+        break;
+        
+    }
+Console.Write($"A^B = {c}");
+}
