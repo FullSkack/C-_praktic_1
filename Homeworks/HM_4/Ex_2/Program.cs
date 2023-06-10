521d5f6aed76d419e539fd0e7d5acd96e32d437c
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-10 23:21:14 +0300

    commit

---
+++ Homeworks/HM_4/Ex_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int CountNum(int num)
+{
+    if(num == 0) return 1;
+
+    int count = 0;
+    while (num != 0)
+    {
+        num/=10;
+        count++;
+    }
+    return count;
+}
+
+int SumNum(int num, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int len = CountNum(num);
+System.Console.WriteLine($"Сумма цифр числа равна = {SumNum(num,len)}");
