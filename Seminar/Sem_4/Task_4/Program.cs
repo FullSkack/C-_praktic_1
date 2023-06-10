07a4b36d48ec7c94fa4de7b2604fb62827a8f6bd
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-10 21:42:09 +0300

    commit

--- Seminar/Sem_4/Task_4/Program.cs
+++ Seminar/Sem_4/Task_4/Program.cs
@@ -1,2 +1,29 @@
 ﻿// Функция выводит массив из 8 лементов случайного набора 0 и 1
-Console.WriteLine("Hello, World!");
+int[] array = new int[8]; // задаем колличесвто эллементов массива
+
+void FillArray(int[] collection) // Объявляем метод и переменные для работы
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length) // цикл перебора и присвоения случайного значения элементу
+    {
+        collection[index] = new Random().Next(0, 2);
+        index ++;
+    }
+}
+
+void PrintArray(int[] massiv) // Объявляем метод и переменные для работы
+{
+    int count = massiv.Length;
+    int position = 0;
+
+    while (position < count) // цикл перебора и вывода в консоль соответсвующего элемента
+    {
+        Console.Write(massiv[position]);
+        position ++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
