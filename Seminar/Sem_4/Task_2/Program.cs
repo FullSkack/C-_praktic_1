d4636d880fde5ba722f8975d5df0daa5c9593cb1
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-10 19:44:15 +0300

    commit

--- Seminar/Sem_4/Task_2/Program.cs
+++ Seminar/Sem_4/Task_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Функция принимает число и выдает сколько в нем цифр
+int CountNum(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num/=10;
+        count++;
+    }
+    return count;
+}
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Колличество Цифр - {CountNum(num)}");
