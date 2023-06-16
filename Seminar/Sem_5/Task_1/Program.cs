a94883214ece29ad7497e24b8d157c152747e941
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-16 09:41:53 +0300

    commit

--- Seminar/Sem_5/Task_1/Program.cs
+++ Seminar/Sem_5/Task_1/Program.cs
@@ -31,18 +31,18 @@ void FinSums(int[] array)
     System.Console.WriteLine($"Сумма положительных чисел: {sumP}");
  }
 
-// int FinSums2(int[] array)
-// {
-//     int sumP = 0; // сумма положительных
-//     int sumN = 0; // сумма отрицательных
-//     foreach (var item in array)
-//         if (item>0)
-//             sumP += item;
-//         else
-//             sumN += item;
-//     int[] result = {sumN, sumP};
-//     return result;
-// }
+int FinSums2(int[] array)
+{
+    int sumP = 0; // сумма положительных
+    int sumN = 0; // сумма отрицательных
+    foreach (var item in array)
+        if (item>0)
+            sumP += item;
+        else
+            sumN += item;
+    int[] result = {sumN, sumP};
+    return result;
+}
 
 Console.Clear();
 System.Console.Write("введите размерность: ");
@@ -51,4 +51,4 @@ int[] array = new int[size];
 FillArray(array);
 PrintArray(array);
 FinSums(array);
-// PrintArray(FinSums2(array));
+PrintArray(FinSums2(array));
---
+++ Seminar/Sem_5/Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Программа меняет положительные числа на соответвующие отрицательные и наоборот
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);      
+    }
+}
+
+void Replace(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;      
+    }
+    System.Console.WriteLine();
+}
+
+void PrintArray(int[] array)
+{
+    // цикл перебора всех элементов без индекса 
+    foreach (var item in array)
+    {
+        System.Console.Write($"{item};");
+    }
+    System.Console.WriteLine();
+}
+
+Console.Clear();
+System.Console.Write("введите размерность: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+FillArray(array);
+PrintArray(array);
+Replace(array);
+PrintArray(array);
