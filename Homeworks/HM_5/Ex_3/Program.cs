738f954b77955de6296b727678739ebd7100f4dc
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-17 01:41:17 +0300

    commit

---
+++ Homeworks/HM_5/Ex_3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив вещественных случайных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+void MinMax(int[] array)
+{
+    int max = 0;
+    int min = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            {
+                max = array[i];
+            }
+        else if (array[i] < min)
+            {
+                min = array[i];
+            }
+    }
+    System.Console.WriteLine($"MAX = {max}");
+    System.Console.WriteLine($"MIN = {min}");
+    System.Console.WriteLine($"MAX - MIN = {max - min}");
+}
+
+Console.Clear(); 
+System.Console.Write("Введите диапазон массива: "); 
+int size = Convert.ToInt32(System.Console.ReadLine()); 
+
+int[] array = new int[size]; 
+
+FillArray(array);
+MinMax(array);
