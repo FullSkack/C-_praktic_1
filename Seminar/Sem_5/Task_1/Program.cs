7ea33b542d141fd17cf0642363e0ae9791cef702
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-16 08:11:26 +0300

    commit

---
+++ Seminar/Sem_5/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Массив из 12 случайных элементов в интервале [-9;9]
+// найдите сумму положительных чисел
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);      
+    }
+}
+
+void PrintArray(int[] array)
+{
+    // цикл перебора всех элементов без индекса 
+    foreach (var item in array)
+    {
+        System.Console.Write($"{item};");
+    }
+    System.Console.WriteLine();
+}
+
+Console.Clear();
+System.Console.Write("введите размерность: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
