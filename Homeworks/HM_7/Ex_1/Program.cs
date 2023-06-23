aa842af97c06d1337890f8e2b4d46e69941824c5
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-24 01:42:35 +0300

    commit

---
+++ Homeworks/HM_7/Ex_1/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray (double[,] array)
+// метод заполняющий массив случайными целыми числами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().NextDouble();
+}
+
+void PrintArray(double[,] array)
+// метод печатающий массив из случайных чисел
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($"{array[i,j],3};  ");
+        System.Console.WriteLine();
+        }
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите кол-во столбцов");
+int cals = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[rows, cals]; // создаем двумерный массив
+
+FillArray(array);
+PrintArray(array);
