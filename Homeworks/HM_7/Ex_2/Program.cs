297c869c721d662a4b11a507553c00fe52bef1fe
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-24 09:17:30 +0300

    commit

--- Homeworks/HM_7/Ex_2/Program.cs
+++ Homeworks/HM_7/Ex_2/Program.cs
@@ -8,10 +8,26 @@
 
 void PrintArray(int[,] array)
 // метод печатающий массив из случайных чисел
-{   for (int j = 0; j < array.GetLength(1); j++)
-            System.Console.Write($"{array[i,j]};");
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    ");
         System.Console.WriteLine();
-    
+        }
+}
+
+void PrintEl(int[,] array, int num)
+// метод работающий с элементом массива
+{   
+    string pos = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        if (array [i,j] == num)
+            pos = $"позиция числа - {i},{j};";
+        else
+            pos = "такого числа в массиве нет;";
+    System.Console.WriteLine(pos);
 }
 
 Console.Clear();
@@ -23,5 +39,9 @@ int cals = Convert.ToInt32(Console.ReadLine());
 
 int[,] array = new int [rows, cals]; // создаем двумерный массив
 
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
 FillArray(array);
 PrintArray(array);
+PrintEl(array, num);
