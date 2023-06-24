e65603486c477f5852c277584de5ac01af7c3a46
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-24 10:17:29 +0300

    commit

---
+++ Homeworks/HM_7/Ex_3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив из целых чисел
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void FillArray (int[,] array)
+// метод заполняющий массив случайными целыми числами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(-30,31);
+}
+
+void PrintArray(int[,] array)
+// метод печатающий массив из случайных чисел
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+void AvgNum(int[,] array)
+// метод определяющий среднее арифметическое.
+{
+    System.Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        Console.Write($"{sum/array.GetLength(0)};");
+    }
+}
+
+
+
+Console.Clear();
+System.Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите кол-во столбцов");
+int cals = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int [rows, cals];
+
+FillArray(array);
+PrintArray(array);
+AvgNum(array);
+
