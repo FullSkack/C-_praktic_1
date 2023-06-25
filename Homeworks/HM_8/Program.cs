040a3070358db52d58cf8ece922e1c040d4b4126
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-25 09:10:11 +0300

    commit

---
+++ Homeworks/HM_8/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(-30,31);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    "); 
+        System.Console.WriteLine();
+        }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+            
+        
+    
+
+
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int [rows, cols];
+
+FillArray(array);
+PrintArray(array);
+SortToLower(array);
+System.Console.WriteLine();
+PrintArray(array);
---
+++ Seminar/Sem_8/Task_1/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(-30,31);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    "); 
+        System.Console.WriteLine();
+        }
+}
+
+void Transponirovanie(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = i; j < array.GetLength(1); j++)
+    {
+        int temp = array[i,j]; // здесь можно было сделать множетвенное присваивание
+        array[i,j] = array[j, i];
+        array [j,i] = temp;
+
+    }     
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int [rows, cols];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+if (array.GetLength(0) == array.GetLength(1))
+{
+    Transponirovanie(array);
+    PrintArray(array);
+}
+else 
+{
+    System.Console.WriteLine("Матрица не квадратная");
+}
