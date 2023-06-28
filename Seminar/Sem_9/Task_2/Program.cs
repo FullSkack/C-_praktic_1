48192604ffe37165d381d3f1c0d86461d26496eb
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-28 22:14:42 +0300

    commit

---
+++ Seminar/Sem_9/Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа выводит значения от 1 до N
+
+string GetNumbers(int n)
+// реализация через бесконечный цикл
+{
+    string str = "";
+    while (true)
+    {
+        str = Convert.ToString(n) + ";" + str;
+        n--;
+        if (n==0)
+            break;
+    }
+    return str;
+}
+
+// реализация через рекурсию
+string GetNumbersRec(int n)
+{
+    if (n == 0)  return "";
+    return GetNumbersRec(n - 1) + n + ";" ;    
+}
+
+Console.Clear();
+
+System.Console.WriteLine("Введите целое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(GetNumbers(n));
+System.Console.WriteLine(GetNumbersRec(n));
