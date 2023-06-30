525ded36bf71656244541781e61c388ed4eb66e0
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-30 18:40:44 +0300

    commit

---
+++ Seminar/Sem_9/Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Summa(int num)
+// реализация через бесконечный цикл
+{
+    int sum = 0;
+    while (true)
+    {
+        sum += num % 10;
+        num /= 10;
+        if (num == 0)
+            break;
+    }
+    return sum;
+}
+
+int SummRec(int num)
+// реализация через рекурсию
+{
+    if(num == 0) return 0;
+    return num % 10 + SummRec(num / 10);
+}
+
+Console.Clear();
+System.Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма цифр числа = {Summa(num)};");
+System.Console.WriteLine($"Сумма цифр числа = {SummRec(num)};");
