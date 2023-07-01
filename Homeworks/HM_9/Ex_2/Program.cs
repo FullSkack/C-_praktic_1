ac00d7f99a7e160b323beeae71dddbef24f563ba
Martin_Power <f.drindrozhik@yandex.ru>
2023-07-01 11:03:23 +0300

    commit

---
+++ Homeworks/HM_9/Ex_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+int SumRec(int n, int m)
+{
+    if(n == m) return m;
+    return n + SumRec (n - 1, m);
+}
+
+Console.Clear();
+
+System.Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(SumRec(n,m));
